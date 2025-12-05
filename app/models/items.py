6bcfd014a6faf652565c0384f78530a1e8989e31
1zana42 <yarik160408@gmail.com>
2025-12-05 11:28:44 +0300

    add database

---
+++ app/models/items.py
@@ -0,0 +1,14 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Float, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.database.database import Base
+
+
+class ItemModel(Base):
+    __tablename__ = "items"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    price: Mapped[int] = mapped_column(nullable=False)
+    quantity: Mapped[int] = mapped_column(nullable=False)
+    characteristics: Mapped[str] = mapped_column(String(255), nullable=False)
--- app/models/users.py
+++ app/models/users.py
@@ -11,9 +11,8 @@ if TYPE_CHECKING:
 class UserModel(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(100), nullable=False)
-    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(300), nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
 
     role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"), nullable=False)
     role: Mapped["RoleModel"] = relationship(back_populates="users")
